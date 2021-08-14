17df2f66ee7111d426794741d148ae96e1668a72
Lun4re <lun4rious@gmail.com>
2021-08-14 19:36:21 +0200

    ye

---
+++ main.js
@@ -0,0 +1,20 @@
+import { differenceInSeconds, intervalToDuration } from "date-fns";
+
+const starterDate = new Date("2021-08-14T16:38:50.875Z");
+
+const calcTimeFromStarterDate = () => {
+  const duration = intervalToDuration({
+    start: 0,
+    end: differenceInSeconds(starterDate, new Date()) * 1000,
+  });
+
+  return `${duration.hours}:${duration.minutes}:${
+    duration.seconds < 10 ? "0" + duration.seconds : duration.seconds
+  }`;
+};
+
+const timerElementNode = document.querySelector(".timer");
+
+setInterval(() => {
+  timerElementNode.innerHTML = calcTimeFromStarterDate();
+}, 1000);
